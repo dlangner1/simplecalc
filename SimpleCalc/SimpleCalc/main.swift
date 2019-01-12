26b0b80b6695de515298e37e0d154b3607ad07d8
Dustin Langner <dustin.langner1@gmail.com>
2019-01-12 15:15:03 -0800

    Add functionality to handle expressions

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -10,10 +10,76 @@ import Foundation
 
 public class Calculator {
     public func calculate(_ args: [String]) -> Int {
+        switch args[args.count - 1] {
+        case "count":
+            return args.count - 1
+        case "avg":
+            return average(args)
+        case "fact":
+            return factorial(args[0])
+        default:
+            return calculateBasicExpression(args)
+        }
     }
     
     public func calculate(_ arg: String) -> Int {
         return calculate( arg.split(separator: " ").map({ substr in String(substr) }) )
+    }
+    
+    fileprivate func calculateBasicExpression(_ args: [String]) -> Int {
+        guard let first = Int(args[0]), let second = Int(args[2]) else {
+            print("unable to calculate expression")
+            return -1
+        }
+        
+        switch args[1] {
+        case "+":
+            return first + second
+        case "-":
+            return first - second
+        case "/":
+            return first / second
+        case "%":
+            return first % second
+        case "*":
+            return first * second
+        default:
+            print("unable to calculate expression")
+            return -1
+        }
+    }
+    
+    fileprivate func average(_ values: [String]) -> Int {
+        if values.count < 2 {
+            return 0
+        }
+        
+        var total = 0
+        for i in 0..<values.count - 1 {
+            guard let intValue = Int(values[i]) else {
+                continue
+            }
+            total += intValue
+        }
+        return total / (values.count - 1)
+    }
+    
+    fileprivate func factorial(_ val: String) -> Int {
+        if val == "fact" {
+            return 0
+        }
+        
+        guard var intValue = Int(val) else {
+            print("unable to calculate expression")
+            return -1
+        }
+        
+        var fact = 1
+        while intValue > 0 {
+            fact *= intValue
+            intValue -= 1
+        }
+        return fact
     }
 }
 
